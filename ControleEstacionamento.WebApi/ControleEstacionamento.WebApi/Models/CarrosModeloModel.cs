8a6507dc9152d310b0c5bde2f2035a09bd760854
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-09 14:43:06 -0300

    Criando classe CarroModeloModel

---
+++ ControleEstacionamento.WebApi/ControleEstacionamento.WebApi/Models/CarrosModeloModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace ControleEstacionamento.WebApi.Models
+{
+    public class CarrosModeloModel
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+    }
+}
