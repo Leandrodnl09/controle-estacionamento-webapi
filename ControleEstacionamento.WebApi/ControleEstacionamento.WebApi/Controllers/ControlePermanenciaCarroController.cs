9644b046d29fae2652f9a183f1f48f0af73b932c
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-09 17:27:06 -0300

    Criando uma controller ControlePermanenciaCarroController e atribuindo veículos para testes

---
+++ ControleEstacionamento.WebApi/ControleEstacionamento.WebApi/Controllers/ControlePermanenciaCarroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstacionamento.WebApi.Controllers
+{
+    public class ControlePermanenciaCarroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
