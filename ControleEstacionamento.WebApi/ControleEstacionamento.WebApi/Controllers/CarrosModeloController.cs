a4bebf8fd30c5c8cdb262f79353851729491e381
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-09 15:22:26 -0300

    Criando uma controller CarrosModeloController e atribuindo veiculos para testes.

---
+++ ControleEstacionamento.WebApi/ControleEstacionamento.WebApi/Controllers/CarrosModeloController.cs
@@ -0,0 +1,45 @@
+﻿using ControleEstacionamento.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstacionamento.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/modelos")]
+    public class CarrosModeloController : Controller
+    {
+        private static List<CarrosModeloModel> _carrosModeloModels = new List<CarrosModeloModel>()
+        {
+            new CarrosModeloModel()
+            {
+                Id = 1,
+                Ano = 2022,
+                Marca = "Fiat",
+                Modelo = "Uno"
+            }
+
+            new CarrosModeloModel()
+            {
+                Id = 2,
+                Ano = 2021,
+                Marca = "Volkswagem",
+                Modelo = "Gol"
+            }
+
+            new CarrosModeloModel()
+            {
+                Id = 3,
+                Ano = 2020,
+                Marca = "Chevrolet",
+                Modelo = "Monza"
+            }
+
+            new CarrosModeloModel()
+            {
+                Id = 4,
+                Ano = 2019,
+                Marca = "Ford",
+                Modelo = "Fiesta"
+            }
+        };
+    }
+}
