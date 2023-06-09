772f6cf5f725ee35b3122f18c83d9b912086c381
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-06-09 17:36:02 -0300

    Atribuindo HttpGet na classe ControlePermanecia para teste no Swegger

--- ControleEstacionamento.WebApi/ControleEstacionamento.WebApi/Controllers/ControlePermanenciaCarroController.cs
+++ ControleEstacionamento.WebApi/ControleEstacionamento.WebApi/Controllers/ControlePermanenciaCarroController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleEstacionamento.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControleEstacionamento.WebApi.Controllers
 {
+    [ApiController]
+    [Route("api/controlepermanencia")]
     public class ControlePermanenciaCarroController : Controller
     {
-        public IActionResult Index()
+        private static List<ControlePermanenciaCarrosModel> controlePermanenciaCarros = new List<ControlePermanenciaCarrosModel>()
         {
-            return View();
+            new ControlePermanenciaCarrosModel()
+            {
+                Id = 1,
+                Placa = "AAA-1111",
+                IdModelo = 001,
+                DataHoraEntrada = new DateTime(2023,06,08,16,03,00)
+            },
+
+            new ControlePermanenciaCarrosModel()
+            {
+                Id = 2,
+                Placa = "AAA-2222",
+                IdModelo = 002,
+                DataHoraEntrada = new DateTime(2023,06,08,16,03,00)
+            },
+
+            new ControlePermanenciaCarrosModel()
+            {
+                Id = 3,
+                Placa = "AAA-3333",
+                IdModelo = 003,
+                DataHoraEntrada = new DateTime(2023,06,08,16,03,00)
+            },
+
+            new ControlePermanenciaCarrosModel()
+            {
+                Id = 4,
+                Placa = "AAA-4444",
+                IdModelo = 004,
+                DataHoraEntrada = new DateTime(2023,06,08,16,03,00)
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(controlePermanenciaCarros);
         }
     }
 }
